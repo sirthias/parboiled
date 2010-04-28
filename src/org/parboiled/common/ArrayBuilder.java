22a82d314ad905edd0d06f7fd4dcfaf7cec3e234
Mathias <mathias@parboiled.org>
2010-04-28 15:39:43 +0200

    Addd org.parboiled.common.ArrayBuilder convenience class

---
+++ src/org/parboiled/common/ArrayBuilder.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2009-2010 Mathias Doenitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.parboiled.common;
+
+import java.lang.reflect.Array;
+
+public class ArrayBuilder<T> {
+
+    private T[] array;
+
+    public ArrayBuilder() {
+        array = null;
+    }
+
+    public ArrayBuilder(T... elements) {
+        array = elements;
+    }
+
+    public T[] get() {
+        return array;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public ArrayBuilder<T> add(T... elements) {
+        if (elements == null) return this;
+        if (array == null) {
+            array = elements;
+            return this;
+        }
+        T[] newArray = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length + elements.length);
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        System.arraycopy(elements, 0, newArray, array.length, elements.length);
+        array = newArray;
+        return this;
+    }
+
+}
