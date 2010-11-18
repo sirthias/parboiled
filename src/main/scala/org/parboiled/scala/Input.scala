95c232b0bee884eb517ed524df6260c3eb157c83
Mathias <mathias@parboiled.org>
2010-11-18 18:17:18 +0100

    Improved scala Input wrapper

--- src/main/scala/org/parboiled/scala/Input.scala
+++ src/main/scala/org/parboiled/scala/Input.scala
@@ -9,7 +9,7 @@ import annotation.tailrec
  * org.parboiled.scala package object.
  */
 class Input(val input: Array[Char], bufferCreator: (Array[Char] => InputBuffer) = new DefaultInputBuffer(_)) {
-  def inputBuffer: InputBuffer = bufferCreator(input)
+  lazy val inputBuffer: InputBuffer = bufferCreator(input)
 
   /**
    * Causes the input to be wrapped with a IndentDedentInputBuffer.
@@ -20,14 +20,13 @@ class Input(val input: Array[Char], bufferCreator: (Array[Char] => InputBuffer) 
    * Collects the actual input text the input buffer provides into a String.
    * This is especially usefull for IndentDedentInputBuffers created by "transformIndents".
    */
-  def collectContents: String = contents(inputBuffer, new StringBuilder, 0).toString
-
-  @tailrec
-  private def contents(buf: InputBuffer, sb: StringBuilder, ix: Int): StringBuilder =
-    buf.charAt(ix) match {
+  def collectContents: String = {
+    @tailrec def contents(buf: InputBuffer, sb: StringBuilder, ix: Int): StringBuilder = buf.charAt(ix) match {
       case Chars.EOI => sb
       case Chars.INDENT => { sb.append('→'); contents(buf, sb, ix + 1) }
       case Chars.DEDENT => { sb.append('←'); contents(buf, sb, ix + 1) }
       case c => { sb.append(c); contents(buf, sb, ix + 1) }
     }
+    contents(inputBuffer, new StringBuilder, 0).toString
+  }
 }
