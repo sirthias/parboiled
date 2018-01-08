41367920d7c8f3f6e70914b89954650f8cc1212d
xuwei-k <6b656e6a69@gmail.com>
2018-01-08 14:36:14 +0900

    fix compile error. update scalatest version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,11 +18,5 @@ object Dependencies {
   val asmUtil     = "org.ow2.asm"   %  "asm-util"      % "5.2"
   val testNG      = "org.testng"    %  "testng"        % "5.14.10"
 
-  def scalatest(scalaVersion: String) = "org.scalatest" %% "scalatest" % {
-    scalaVersion match {
-      case "2.10.4" => "2.2.4"
-      case "2.11.8" => "2.2.4"
-      case "2.12.1" => "3.0.1"
-    }
-  }
+  def scalatest(scalaVersion: String) = "org.scalatest" %% "scalatest" % "3.0.4"
 }
