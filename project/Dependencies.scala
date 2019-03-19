52c046f9c77621963fa67bdc1ec885192ffe8e5c
kenji yoshida <6b656e6a69@gmail.com>
2019-03-19 15:47:41 +0900

    Update scalatest to 3.0.7 (#134)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,9 +20,6 @@ object Dependencies {
   val testNG      = "org.testng"    %  "testng"        % "6.14.3"
 
   def scalatest(scalaVersion: String) = {
-    if (scalaVersion == "2.13.0-M5")
-      Seq("org.scalatest" %% "scalatest" % "3.0.6-SNAP3")
-    else
-      Seq("org.scalatest" %% "scalatest" % "3.0.5")
+    Seq("org.scalatest" %% "scalatest" % "3.0.7")
   }
 }
