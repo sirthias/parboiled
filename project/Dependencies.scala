2c1bc1384a567811ba4fc7b7940490b003c33ed8
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2021-03-08 05:04:59 +0000

    Update scalatest to 3.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,6 +20,6 @@ object Dependencies {
   val testNG      = "org.scalatestplus" %% "testng-6-7" % "3.2.5.0"
 
   def scalatest(scalaVersion: String) = {
-    Seq("org.scalatest" %% "scalatest" % "3.2.5")
+    Seq("org.scalatest" %% "scalatest" % "3.2.6")
   }
 }
