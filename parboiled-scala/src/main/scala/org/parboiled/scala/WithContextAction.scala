5343997b7158dcbfa2a0e57a2c24b3e7b1697dc7
kenji yoshida <6b656e6a69@gmail.com>
2023-09-08 09:53:27 +0900

    add explicit type

--- parboiled-scala/src/main/scala/org/parboiled/scala/WithContextAction.scala
+++ parboiled-scala/src/main/scala/org/parboiled/scala/WithContextAction.scala
@@ -19,29 +19,29 @@ package org.parboiled.scala
 import org.parboiled.Context
 
 class WithContextAction1[A, R](val action: (A, Context[Any]) => R) extends (A => R) {
-  def apply(a: A) = throw new UnsupportedOperationException
+  def apply(a: A): R = throw new UnsupportedOperationException
 }
 
 class WithContextAction2[A, B, R](val action: (A, B, Context[Any]) => R) extends ((A, B) => R) {
-  def apply(a: A, b: B) = throw new UnsupportedOperationException
+  def apply(a: A, b: B): R = throw new UnsupportedOperationException
 }
 
 class WithContextAction3[A, B, C, R](val action: (A, B, C, Context[Any]) => R) extends ((A, B, C) => R) {
-  def apply(a: A, b: B, c: C) = throw new UnsupportedOperationException
+  def apply(a: A, b: B, c: C): R = throw new UnsupportedOperationException
 }
 
 class WithContextAction4[A, B, C, D, R](val action: (A, B, C, D, Context[Any]) => R) extends ((A, B, C, D) => R) {
-  def apply(a: A, b: B, c: C, d: D) = throw new UnsupportedOperationException
+  def apply(a: A, b: B, c: C, d: D): R = throw new UnsupportedOperationException
 }
 
 class WithContextAction5[A, B, C, D, E, R](val action: (A, B, C, D, E, Context[Any]) => R) extends ((A, B, C, D, E) => R) {
-  def apply(a: A, b: B, c: C, d: D, e: E) = throw new UnsupportedOperationException
+  def apply(a: A, b: B, c: C, d: D, e: E): R = throw new UnsupportedOperationException
 }
 
 class WithContextAction6[A, B, C, D, E, F, R](val action: (A, B, C, D, E, F, Context[Any]) => R) extends ((A, B, C, D, E, F) => R) {
-  def apply(a: A, b: B, c: C, d: D, e: E, f: F) = throw new UnsupportedOperationException
+  def apply(a: A, b: B, c: C, d: D, e: E, f: F): R = throw new UnsupportedOperationException
 }
 
 class WithContextAction7[A, B, C, D, E, F, G, R](val action: (A, B, C, D, E, F, G, Context[Any]) => R) extends ((A, B, C, D, E, F, G) => R) {
-  def apply(a: A, b: B, c: C, d: D, e: E, f: F, g: G) = throw new UnsupportedOperationException
+  def apply(a: A, b: B, c: C, d: D, e: E, f: F, g: G): R = throw new UnsupportedOperationException
 }
